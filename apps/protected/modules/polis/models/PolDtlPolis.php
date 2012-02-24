6a62d950946b1a57657f37d335a5eafb90281e60
Abdul Ibad <abdul.ibad@dulabs.com>
2012-02-24 22:48:02 +0700

    add master polis

---
+++ apps/protected/modules/polis/models/PolDtlPolis.php
@@ -0,0 +1,10 @@
+<?php
+
+Yii::import('application.modules.polis.models._base.BasePolDtlPolis');
+
+class PolDtlPolis extends BasePolDtlPolis
+{
+	public static function model($className=__CLASS__) {
+		return parent::model($className);
+	}
+}
